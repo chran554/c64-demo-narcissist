6a5ddb6d3abb8657f9565d1123d782aea9f79b84
Christian Andersson <chran554@hotmail.com>
2022-10-04 14:35:07 +0000

    Add documentation.

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -10,4 +10,4 @@ cd $projectFileDir
 projectFileDir=$(pwd)
 cd -
 
-"$utilities_path/compile_and_debug.sh" "$assemblerFile" "$projectFileDir" debug
+"$utilities_path/compile_and_debug.sh" "$assemblerFile"
